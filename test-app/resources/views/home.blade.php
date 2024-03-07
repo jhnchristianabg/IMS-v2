e2addcb74d3e59c44c19d05414f1833af88d23ea
John Christian Abueg <jhnchristian.abueg@gmail.com>
2024-03-07 16:36:37 +0800

    Dashboard

--- test-app/resources/views/home.blade.php
+++ test-app/resources/views/home.blade.php
@@ -1,15 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
+    <h1><b>Dashboard</b></h1>
+
+    <div class="row justify ">
+      <div class="col-md-12 mb-4">
+          <div class="card bg-success text-white" style="width: 100%; height: 25vh">
+
+              <div class="card-body">
+                  @if (session('status'))
+                      <div class="alert alert-success" role="alert">
+                          {{ session('status') }}
+                      </div>
+                  @endif
+
+                  <h3 style="margin-left:50px; margin-top:10px; font-family:Arial, Helvetica, sans-serif">Welcome to Inventory Management System</h3>
+
+                  <a href="#" class="btn btn-warning" style="margin-left:50px; margin-top:80px">See All</a>
+              </div>
+          </div>
+      </div>
+    </div>
+
         <div class="row">
             <div class="col-sm-4">
               <div class="card">
                 <div class="card-body">
                     <h5 class="card-title"><b>Devices</b>
-                        <a href="#" class="btn btn-success" style="margin-left:100px;">Add</a>
-                        <a href="#" class="btn btn-success">Deploy</a>
-                        <a href="#" class="btn btn-success">Pullout</a>
+                        <a href="#" class="btn btn-warning" style="margin-left:100px;">Add</a>
+                        <a href="#" class="btn btn-warning">Deploy</a>
+                        <a href="#" class="btn btn-warning">Pullout</a>
                     </h5>
                     <p class="card-text">List of all Devices</p>
 
@@ -52,9 +74,9 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title"><b>Cables & Peripherals</b>
-                            <a href="#" class="btn btn-success">Add</a>
-                            <a href="#" class="btn btn-success">Deploy</a>
-                            <a href="#" class="btn btn-success">Pullout</a>
+                            <a href="#" class="btn btn-warning">Add</a>
+                            <a href="#" class="btn btn-warning">Deploy</a>
+                            <a href="#" class="btn btn-warning">Pullout</a>
                         </h5>
                         <p class="card-text">List of all Cables and Peripherals</p>
 
@@ -97,9 +119,9 @@
                 <div class="card">
                   <div class="card-body">
                       <h5 class="card-title"><b>Consumables</b>
-                          <a href="#" class="btn btn-success" style="margin-left:60px;">Add</a>
-                          <a href="#" class="btn btn-success">Deploy</a>
-                          <a href="#" class="btn btn-success">Pullout</a>
+                          <a href="#" class="btn btn-warning" style="margin-left:60px;">Add</a>
+                          <a href="#" class="btn btn-warning">Deploy</a>
+                          <a href="#" class="btn btn-warning">Pullout</a>
                       </h5>
                       <p class="card-text">List of all Consumables</p>
 
